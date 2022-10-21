f1a6fe4717e30ebbf88aa6b3bead86318290d4d5
SoulLeaf <kittipat.sa@kkumail.com>
2022-10-21 13:58:19 +0700

    init

---
+++ js/music-list.js
@@ -0,0 +1,40 @@
+/// Music List
+
+let allMusic = [
+  {
+    name: "What's Up Danger",
+    artist: "Blackway, Black Caviar",
+    img: "Music10",
+    src: "Music10"
+  },
+  {
+    name: "Spirits",
+    artist: "The Strumbellas",
+    img: "Music6",
+    src: "Music6"
+  },
+  {
+    name: "Nothing Like This",
+    artist: "ALLISTER X",
+    img: "Music1",
+    src: "Music1"
+  },
+  {
+    name: "Hit the Road Jack",
+    artist: "Ray Charles",
+    img: "Music3",
+    src: "Music3"
+  },
+  {
+    name: "Beautiful Girls",
+    artist: "Sean Kingston",
+    img: "Music8",
+    src: "Music8"
+  },
+  {
+    name: "Some Nights",
+    artist: "Fun.",
+    img: "Music9",
+    src: "Music9"
+  },
+];
